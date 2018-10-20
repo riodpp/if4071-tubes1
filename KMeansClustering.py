37115dcf88f4b79a35b513ca55df0c631b11be9e
Rio Dwi Putra <dwiputra.rio@gmail.com>
2018-10-21 06:12:58 +0700

    Dibagian Kmeans kurang ()

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -6,6 +6,10 @@ def jarak(a,b, ax = 1):
     return np.linalg.norm(a-b,axis = ax)
 
 class KMeans:
+    dataset = None
+    labels = None
+    centroid = None
+
     def __init__(self, nkluster):
         self.nkluster = nkluster
 
@@ -14,7 +18,7 @@ class KMeans:
         self.dataset_length = len(dataset)
         self.labels = np.zeros(self.dataset_length)
         self.centroid = self.__init_centroid()
-        self.__KMeans
+        self.__KMeans()
 
     def __init_centroid(self):
         c_x = np.random.randint(0, np.max(self.dataset)-20, size = self.nkluster)
@@ -23,22 +27,21 @@ class KMeans:
         return C
 
     def __KMeans(self):
-        C = self.centroid
-        C_old = np.zeros(C.shape)
+        C_old = np.zeros(self.centroid.shape)
         self.labels = np.zeros(self.dataset_length)
-        error = jarak(C, C_old, None)
+        error = jarak(self.centroid, C_old, None)
         while error != 0:
             # memasukkan setiap titik ke cluster terdekatnya
             # Assigning each value to its closest cluster
             for i in range(len(self.dataset)):
-                distances = jarak(self.dataset[i], C)
+                distances = jarak(self.dataset[i], self.centroid)
                 cluster = np.argmin(distances)
                 self.labels[i] = cluster
-            C_old = deepcopy(C)
+            C_old = deepcopy(self.centroid)
             for i in range(self.nkluster):
                 points = [self.dataset[j] for j in range(len(self.dataset)) if self.labels[j] == i]
-                C[i] = np.mean(points, axis=0)
-            error = jarak(C, C_old, None)
+                self.centroid[i] = np.mean(points, axis=0)
+            error = jarak(self.centroid, C_old, None)
 
 
 
