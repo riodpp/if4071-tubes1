9f4873b52f79ca958a234ba23034f996bedbe3be
Rio Dwi Putra <dwiputra.rio@gmail.com>
2018-10-21 07:54:53 +0700

    Perbaikan algoritma agar bisa digunakan dengan dataset iris

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -2,9 +2,6 @@ from copy import deepcopy
 import numpy as np
 import pandas as pd
 
-def jarak(a,b, ax = 1):
-    return np.linalg.norm(a-b,axis = ax)
-
 class KMeans:
     dataset = None
     labels = None
@@ -15,33 +12,32 @@ class KMeans:
 
     def fit(self, dataset):
         self.dataset = dataset
-        self.dataset_length = len(dataset)
-        self.labels = np.zeros(self.dataset_length)
+        self.dataset_shape = dataset.shape
+        self.labels = np.zeros(self.dataset_shape[0])
         self.centroid = self.__init_centroid()
         self.__KMeans()
 
     def __init_centroid(self):
-        c_x = np.random.randint(0, np.max(self.dataset)-20, size = self.nkluster)
-        c_y = np.random.randint(0, np.max(self.dataset)-20, size = self.nkluster)
-        C = np.array(list(zip(c_x,c_y)), dtype = np.float32)
+        mean = np.mean(self.dataset, axis = 0)
+        std = np.std(self.dataset, axis = 0)
+        C = np.random.randn(self.nkluster,self.dataset_shape[1])*std + mean
         return C
 
     def __KMeans(self):
         C_old = np.zeros(self.centroid.shape)
-        self.labels = np.zeros(self.dataset_length)
-        error = jarak(self.centroid, C_old, None)
+        distances = np.zeros((self.dataset_shape[0],self.nkluster))
+        error = np.linalg.norm(self.centroid - C_old)
         while error != 0:
             # memasukkan setiap titik ke cluster terdekatnya
             # Assigning each value to its closest cluster
-            for i in range(len(self.dataset)):
-                distances = jarak(self.dataset[i], self.centroid)
-                cluster = np.argmin(distances)
-                self.labels[i] = cluster
+            for i in range(self.nkluster):
+                distances[:,i] = np.linalg.norm(self.dataset - self.centroid[i], axis=1)
+                self.labels = np.argmin(distances, axis=1)
             C_old = deepcopy(self.centroid)
             for i in range(self.nkluster):
-                points = [self.dataset[j] for j in range(len(self.dataset)) if self.labels[j] == i]
-                self.centroid[i] = np.mean(points, axis=0)
-            error = jarak(self.centroid, C_old, None)
+                #points = [self.dataset[j] for j in range(len(self.dataset)) if self.labels[j] == i]
+                self.centroid[i] = np.mean(self.dataset[self.labels==i], axis=0)
+            error = np.linalg.norm(self.centroid - C_old)
 
 
 
